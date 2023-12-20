1e8a8c10cfc8da0616326431dd498d0d49b8f868
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-12-20 18:06:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -98,19 +98,25 @@ with dpg.window(tag="Primary Window"):
     dpg.add_input_text(label="Output", tag="output", readonly=True, multiline=True, height=146)
 
 with dpg.theme() as global_theme:
-    with dpg.theme_component(dpg.mvAll):
-        dpg.add_theme_style(dpg.mvStyleVar_WindowRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_GrabRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_TabRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_ChildRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_PopupRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_ScrollbarRounding, 3)
-        dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 4, 4)
-        dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 5, 5)
+    with dpg.theme() as global_theme:
+        with dpg.theme_component(dpg.mvAll):
+            dpg.add_theme_style(dpg.mvStyleVar_WindowRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_GrabRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_TabRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_ChildRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_PopupRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_ScrollbarRounding, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 5, 5)
+            dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 5, 5)
 
-dpg.bind_theme(global_theme)
-dpg.create_viewport(title='Keyword searcher', width=700, height=385)
+            dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (21, 22, 23))
+            dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (32, 50, 77))
+            dpg.add_theme_color(dpg.mvThemeCol_Button, (39, 73, 114))
+            dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (32, 50, 77))
+
+    dpg.bind_theme(global_theme)
+dpg.create_viewport(title='Keyword searcher', width=700, height=400)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
